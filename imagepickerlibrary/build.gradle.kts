d74459e7369e5fe9a250646598f2527bb12d3c87
osman-teamswork <osman@teamswork.in>
2025-07-28 15:31:54 +0530

    Updated dependencies.

--- imagepickerlibrary/build.gradle.kts
+++ imagepickerlibrary/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.ucrop)
     implementation(libs.coil)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.exifinterface)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
